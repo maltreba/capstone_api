2c3ea3800741ecdf0cdc9998353e64acc7ff6569
maltreba <maltreba@gmail.com>
2020-11-28 15:54:10 +0700

    recode app

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 #home root
 @app.route('/')
 def homepage():
-    return render_template('index.html')
+    return "Welcome"
 
 # get all media types sold by store
 @app.route('/playlist')
@@ -27,7 +27,7 @@ def topbuyer(country, sel_year):
     GROUP by c.CustomerId 
     '''
     data=pd.read_sql_query(querytop,conn)
-    condition1=data['Year']==str(sel_year)
+    condition1=data['Year']==sel_year
     condition2=data['Country']==country
     data=data[condition1&condition2]
     return data.to_json()
